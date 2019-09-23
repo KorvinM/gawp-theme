5c8cf98e5c578cbd3e81ca46eb1d17ab5778e294
KorvinM <km@korvin.org>
2019-09-23 13:56:12 +0100

    update config-sample.js

--- gulpfile.js/config-sample.js
+++ gulpfile.js/config-sample.js
@@ -1,6 +1,6 @@
-const Config ={
+const config ={
   src         : 'src/',//this line should not be touched
   build       : 'YOUR_BUILD_DIRECTORY_HERE',
   wp          : 'WORDPRESS-URL_HERE'
 };
-module.exports = Config;
+module.exports = config;
